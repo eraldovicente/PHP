ff101151e44db8c9a9ac8818db0f29afe2ecc29e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-15 19:17:27 -0300

    Aula-1(introducao, variaveis, tipos)

---
+++ AULA 1 (Introdução, Variáveis, Tipos)/teste.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 'Fazendo inclusão';
+
+// $minha_var;              null
+// $texto = "Olá mundo!";   string 
+// $x = 5;                  integer
+// $y = 10.5;               float ou double
+// $verdadeiro = true;      bollean
+// $nulo = null;            null
